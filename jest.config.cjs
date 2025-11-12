a85416365d0ae758a621e943e2416be768a6cee5
Sabrina <sabrinalamprecht92@gmail.com>
2025-11-12 09:57:40 -0800

    Continuous Integration

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+// Configuration for jest - telling it to use the TypeScript version of jest
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", "src"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
+};
